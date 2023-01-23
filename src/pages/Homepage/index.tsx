6654b216efa51b5af90827cae1f7a63b9f68a682
Lucas Silva <lsengenheirocivil@gmail.com>
2023-01-23 07:09:28 -0300

    feat: create calculation request

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,15 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ContainerInput from "../../components/ContainerInput";
 import ContainerResults from "../../components/ContainerResults";
 import Input from "../../components/Input";
 import MainContainer from "../../components/MainContainer";
+import api from "../../services/api";
 import Container from "./styles";
 
 const Homepage = () => {
+  interface IResults {
+    amount: number,
+    installments: number,
+    mdr: number,
+  }
+
   const [ amount, setAmout ] = useState<number>(0);
   const [ installments, setInstallments ] = useState<number>(0);
   const [ mdr, setMdr ] = useState<number>(0);
   const [ days, setDays ] = useState<number[]>([0, 15, 30, 90]);
+  const [ results, setResults ] = useState<IResults>({} as IResults);
+
+  useEffect(() => {
+    const data = {
+      amount: amount * 100,
+      installments,
+      mdr,
+      days
+    }
+
+    api.post("?delay=1000", data)
+      .then(res => res.data)
+      .then((res) => setResults(res))
+  }, [amount, installments, mdr]);
 
   return (
     <Container>
@@ -38,13 +59,13 @@ const Homepage = () => {
         <ContainerResults>
           <p className="title-results">VOCÊ RECEBERÁ:</p>
           <div className="results">
-            <span>Amanhã: </span>
+            <span>Amanhã: R$ {Object.values(results)[0] / 100}</span>
 
-            <span>Em 15 dias: </span>
+            <span>Em 15 dias: R$ {Object.values(results)[1] / 100}</span>
 
-            <span>Em 30 dias: </span>
+            <span>Em 30 dias: R$ {Object.values(results)[2] / 100}</span>
 
-            <span>Em 90 dias: </span>
+            <span>Em 90 dias: R$ {Object.values(results)[3] / 100}</span>
           </div>
         </ContainerResults>
       </MainContainer>
