6679cbac40fb18b5b7b46f7c5d538360b9f7a48b
Lucas Silva <lsengenheirocivil@gmail.com>
2023-01-22 22:52:18 -0300

    feat: create input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,13 @@
+import Container from "./styles"
+
+const Input = () => {
+  return (
+    <Container>
+      <label htmlFor="">Teste</label>
+      <input type="text" placeholder="" id="" />
+      <span className="legend"></span>
+    </Container>
+  )
+}
+
+export default Input;
---
+++ src/components/Input/styles.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+const Container = styled.fieldset`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  border: none;
+  width: 90%;
+  max-width: 18.75rem;
+  gap: 0.25rem;
+
+  label {
+    color: var(--grey-3);
+  }
+
+  input {
+    width: 100vw;
+    padding: 0.5rem;
+    border: solid 1px var(--grey-2);
+    border-radius: 0.5rem;
+  }
+
+  .legend {
+    font-size: 0.7rem;
+    color: var(--grey-3);
+  }
+`;
+
+export default Container;
